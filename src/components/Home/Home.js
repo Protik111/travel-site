63cac15ef866fc66e9c969e374379d9f6f9cc361
Protik111 <rafiurprotik111.gmail.com>
2021-02-07 13:58:17 +0600

    places functionality added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../Image/Logo2.png';
 import { Button, Navbar, Form, FormControl, Nav } from 'react-bootstrap';
 import '../Home/Home.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useHistory } from 'react-router-dom';
+import Place from '../Place/Place';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
+
 
 const Home = () => {
+    const data = [
+        {
+            id : 1,
+            name : "COX'S BAZAR",
+            description : "Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south.",
+            image : "https://i.ibb.co/3kpVYs6/cox.png"
+        },
+        {
+            id : 2,
+            name : "BANDARBAN",
+            description : "Bandarban is a district in South-Eastern Bangladesh, and a part of the Chittagong Division. It is one of the three hill districts of Bangladesh and a part of the",
+            image : "https://i.ibb.co/RDS6TF7/Sreemongol.png"
+        },
+        {
+            id : 3,
+            name : "SAJEK",
+            description : "Sajek an emerging tourist spot in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in Rangamati District. Sajek valley is known as the Queen of Hills & Roof of Rangamati.",
+            image : "https://i.ibb.co/HX4STdp/sundorbon.png"
+        }
+    ]
+
+    const history = useHistory();
+
+    const [selectedPlace, setSelectedPlace] = useState(data[0]);
+
+    const handleClick = (data) => {
+        setSelectedPlace(data);
+    }
+    const handleBooking =() => {
+        history.push( `/booking/${selectedPlace.id}`);
+    }
+
     return (
         <div className="home">
             <Navbar variant="light" className="nav-container">
@@ -26,22 +57,19 @@ const Home = () => {
                 </Nav>
             </Navbar>
 
-            
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={4}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                ...
-            </Swiper>
+            <div className="row places-row">
+                <div className="col-md-4 heading">
+                    <h1>{selectedPlace.name}</h1>
+                    <p>{selectedPlace.description}</p>
+                    <button onClick={handleBooking} className="booking-btn">Booking <ArrowRightAltIcon></ArrowRightAltIcon></button>
+                </div>
+                <div className="pictures">
+                {
+                    data.map(place => <Place handleClick={handleClick} selectedPlace={selectedPlace} data={place}></Place>)
+                }
+                </div>
+            </div>
+
         </div>
     );
 };
---
+++ src/components/Place/Place.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../components/Place/Place.css';
+
+const Place = (props) => {
+    const handleClick = props.handleClick;
+    const selectedPlace = props.selectedPlace;
+    const data = props.data;
+    console.log('selected',selectedPlace);
+    console.log('click',handleClick);
+    console.log('data', data);
+    return (
+            <div className={`img-box ${selectedPlace.id === data.id && "active-place"}`} onClick={() => handleClick(data)} >
+                <img src={data.image} alt=""/>
+                <h4 className="placeName">{data.name}</h4>
+             </div>
+    );
+};
+
+export default Place;
